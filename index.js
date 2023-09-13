4647ccd9c00d8c63db771588206fe7350adbeddf
tope101 <temitope@kobo360.com>
2023-09-13 10:31:42 +0100

    ahhhh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const personRoutes = require('./routes/personRoutes.js')
 
 app.use(express.json())
